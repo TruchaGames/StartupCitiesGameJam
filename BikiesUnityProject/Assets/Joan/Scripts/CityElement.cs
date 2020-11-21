c25e925ce277f7950dd76fcacca22d285e20fa79
lucho1 <luchosuaya@hotmail.com>
2020-11-21 09:54:33 +0100

    Randomized CityElements height

--- BikiesUnityProject/Assets/Joan/Scripts/CityElement.cs
+++ BikiesUnityProject/Assets/Joan/Scripts/CityElement.cs
@@ -19,7 +19,16 @@ public class CityElement : MonoBehaviour
     void Start()
     {
         //Debug purpose: Update sphere size
-        area.transform.localScale = new Vector3(radius, radius, radius);
+        Vector3 scale = area.transform.localScale;
+        Vector3 pos = area.transform.position;
+
+        //scale.y = Random.Range(1.1f, 7.82f);
+        scale.y = Random.Range(0.3f, 5.0f);
+        pos.y += scale.y/2;
+
+        area.transform.position = pos;
+        area.transform.localScale = scale;
+
         area.SetActive(false);
     }
 }

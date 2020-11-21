4ad5cba02560ab3195ddf96bfd721b3be63a5bb7
Dídac Romero <36265669+DidacRomero@users.noreply.github.com>
2020-11-21 20:23:01 +0100

    Added bicycle flip + some placeholders

---
+++ BikiesUnityProject/Assets/Scripts/bicycle_flipper.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bicycle_flipper : MonoBehaviour
+{
+    public Sprite leftSprite;
+    public Sprite rightSprite;
+
+    public bool go_left = true;
+
+    private SpriteRenderer renderized_sprite;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        renderized_sprite = transform.GetComponent<SpriteRenderer>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (go_left)
+            renderized_sprite.sprite = leftSprite;
+        else
+            renderized_sprite.sprite = rightSprite;
+    }
+}

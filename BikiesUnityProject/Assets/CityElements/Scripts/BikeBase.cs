c27d75fe9a7f0ad67db5a0e51877477a1bb22c72
Carles Homs <36209557+ch0m5@users.noreply.github.com>
2020-11-21 11:29:23 +0100

    Bike Base Prefab + Script Changes

--- BikiesUnityProject/Assets/CityElements/Scripts/BikeBase.cs
+++ BikiesUnityProject/Assets/CityElements/Scripts/BikeBase.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class BikeBase : MonoBehaviour
 {
+    [Header("Bikes")]
     public uint bikeStock = 5;
+
+    [Header("Radius Detection")]
+    public float bikeBaseDetectRadius = 0.0f;
+    public float apartmentDetectRadius = 0.0f;
+    public float interestPointDetectRadius = 0.0f;
+
+    [Header("Lists of Nearby Nodes")]
+    public List<BikeBase> nearbyBikeBases = new List<BikeBase>();
+    public List<Apartment> nearbyApartments = new List<Apartment>();
+    public List<InterestPoint> nearbyInterestPoints = new List<InterestPoint>();
 
     // Start is called before the first frame update
     void Start()
@@ -16,5 +27,43 @@ public class BikeBase : MonoBehaviour
     void Update()
     {
         
+    }
+
+    uint SearchBikeBases()
+    {
+        //// 1. Remove current nodes listed
+        //float nodesConnected = 0.0f;
+        //nearbyBikeBases.Clear();
+
+        //// 2. Iterate list of all existing bike bases
+        //foreach (BikeBase bikeBase in cityManager.activeBikeBases)
+        //{
+        //    // 3. Do a A->B vector from this base to each existing base
+        //    float x_distance = bikeBase.gameObject.transform.position.x - gameObject.transform.position.x;
+        //    float y_distance = bikeBase.gameObject.transform.position.y - gameObject.transform.position.y;
+
+        //    // 4. If magnitude of A->B is <= to radius, then add to correspodant list
+        //    if (Mathf.Sqrt(Mathf.Pow(x_distance, 2) + Mathf.Pow(y_distance, 2)) <= bikeBaseDetectRadius)
+        //    {
+        //        nearbyBikeBases.Add(bikeBase);
+
+        //        // 5. Also, add yourself to the list of other newly connected nodes
+        //        bikeBase.nearbyBikeBases.Add(this);
+
+        //        ++nodesConnected;
+        //    }
+        //}
+
+        //return nodesConnected;
+    }
+
+    uint SearchApartements()
+    {
+        return 0;
+    }
+
+    uint SearchInterestPoint()
+    {
+        return 0;
     }
 }

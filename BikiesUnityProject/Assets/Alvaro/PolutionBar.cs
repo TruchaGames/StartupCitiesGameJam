f6d8980b31ef2ed17fa2602eca30c0e8441ea747
Trikyname <alvarocolomvidal@gmail.com>
2020-11-21 11:55:50 +0100

    Añadida Carpeta

---
+++ BikiesUnityProject/Assets/Alvaro/PolutionBar.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PolutionBar : MonoBehaviour
+{
+
+    public Slider slider;
+
+    private int uiPolution;
+    private int currentPolution = 50;
+    public int maxPolution = 100;
+
+    public GameObject winScreen;
+    public GameObject loseScreen;
+
+    private void Awake()
+    {
+
+        SetMaxPolution(maxPolution);
+    }
+
+
+    // this is only for debug
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            currentPolution += 5;
+            SetPolution(currentPolution);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+
+            currentPolution -= 5;
+            SetPolution(currentPolution);
+        }
+
+        if (currentPolution <= slider.minValue)
+        {
+            LoseCondition();
+        }
+        else if (currentPolution >= slider.maxValue)
+        {
+            WinCondition();
+        }
+
+        //SetPolution(uiPolution);
+    }
+
+    public void SetPolution(int polution)
+    {
+        slider.value = polution;
+    }
+
+    public void SetMaxPolution(int maxPolution)
+    {
+        slider.maxValue = maxPolution;
+        currentPolution = (int)(maxPolution * 0.5f);
+        uiPolution = (int)(maxPolution * 0.5f);
+        Debug.Log(currentPolution + " " + uiPolution);
+    }
+
+    void CurrentPolution(int globalPolution)
+    {
+        uiPolution = globalPolution;
+    }
+
+    public void LoseCondition()
+    {
+        if (!loseScreen.activeSelf)
+        {
+            loseScreen.SetActive(true);
+        }
+
+        gameObject.SetActive(false);
+    
+    }
+
+
+    public void WinCondition()
+    {
+        if (!winScreen.activeSelf)
+        {
+            winScreen.SetActive(true);
+        }
+
+        gameObject.SetActive(false);
+
+    }
+
+
+}

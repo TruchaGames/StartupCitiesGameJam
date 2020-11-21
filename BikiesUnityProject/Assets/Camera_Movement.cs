1b40ed84db5910c113015d5f7bd6bde3990822de
lucho1 <luchosuaya@hotmail.com>
2020-11-21 22:47:20 +0100

    Improved a bit the camera movement

--- BikiesUnityProject/Assets/Camera_Movement.cs
+++ BikiesUnityProject/Assets/Camera_Movement.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class Camera_Movement : MonoBehaviour
 {
-    public int mov_speed = 3;
-    public int zoom_speed = 2000000;
+    [Header("Camera Speeds")]
+    public float mov_speed = 3.0f;
+    public float zoom_speed = 1.0f;
+    public float PanSpeed = 2.0f;
 
+    [Header("Camera Zoom")]
     public int max_zoom_in = 6;
     public int max_zoom_out = 20;
+    
+    [Header("Camera Boundaries")]
+    public float[] BoundariesZ = new float[] { -125f, 22f };
+    public float[] BoundariesX = new float[] { -50f, 69f };
+
+
+    Vector3 lastPosition = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
@@ -19,25 +29,40 @@ public class Camera_Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Camera Panning
+        if (Input.GetMouseButtonDown(2))
+            lastPosition = Input.mousePosition;
+        else if (Input.GetMouseButton(2))
+        {
+            Vector3 offset = GetComponent<Camera>().ScreenToViewportPoint(lastPosition - Input.mousePosition);
+            Vector3 Move = new Vector3(offset.x * PanSpeed, 0, offset.y * PanSpeed);
+            transform.Translate(Move, Space.World);
+        }
+
+        lastPosition = Input.mousePosition;
 
         //GET WASD to move the camera in each axis
-        if(Input.GetKey(KeyCode.A))
-            transform.position = new Vector3(transform.position.x - mov_speed * Time.deltaTime, transform.position.y, transform.position.z);
+        float mSpeed = mov_speed;
+        if (Input.GetKey(KeyCode.LeftShift))
+            mSpeed = mov_speed * 2.0f;
+
+        if (Input.GetKey(KeyCode.A))
+            transform.position = new Vector3(transform.position.x - mSpeed * Time.deltaTime, transform.position.y, transform.position.z);
         if (Input.GetKey(KeyCode.D))
-            transform.position = new Vector3(transform.position.x + mov_speed * Time.deltaTime, transform.position.y, transform.position.z);
+            transform.position = new Vector3(transform.position.x + mSpeed * Time.deltaTime, transform.position.y, transform.position.z);
         if (Input.GetKey(KeyCode.W))
-            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + mov_speed * Time.deltaTime);
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + mSpeed * Time.deltaTime);
         if (Input.GetKey(KeyCode.S))
-            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - mov_speed * Time.deltaTime);
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - mSpeed * Time.deltaTime);
 
         //Use mousewheel to zoom in/out
-        if ( Input.GetAxis("Mouse ScrollWheel") > 0f)   //Mouse_Wheel Forward
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)   //Mouse_Wheel Forward
         {
             float final_pos = transform.position.y - zoom_speed * Time.deltaTime;
             if (final_pos <= max_zoom_in)
                 transform.position = new Vector3(transform.position.x, max_zoom_in, transform.position.z);
             else
-            transform.position = new Vector3(transform.position.x, transform.position.y - zoom_speed*Time.deltaTime, transform.position.z);
+                transform.position = new Vector3(transform.position.x, transform.position.y - zoom_speed*Time.deltaTime, transform.position.z);            
         }
         if (Input.GetAxis("Mouse ScrollWheel") < 0f)   //Mouse_Wheel backward
         {
@@ -45,7 +70,15 @@ public class Camera_Movement : MonoBehaviour
             if (final_pos >= max_zoom_out)
                 transform.position = new Vector3(transform.position.x, max_zoom_out, transform.position.z);
             else
-            transform.position = new Vector3(transform.position.x, transform.position.y + zoom_speed * Time.deltaTime, transform.position.z);
+                transform.position = new Vector3(transform.position.x, transform.position.y + zoom_speed * Time.deltaTime, transform.position.z);
         }
+
+        //GetComponent<Camera>().fieldOfView = Mathf.Clamp(GetComponent<Camera>().fieldOfView - (Input.GetAxis("Mouse ScrollWheel") * zoom_speed), max_zoom_in, max_zoom_out);
+
+        // Camera Boundaries Clamp
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(transform.position.x, BoundariesX[0], BoundariesX[1]);
+        pos.z = Mathf.Clamp(transform.position.z, BoundariesZ[0], BoundariesZ[1]);
+        transform.position = pos;
     }
 }

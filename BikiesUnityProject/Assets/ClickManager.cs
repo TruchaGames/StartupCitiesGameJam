283ef2e833b38a239cb1325e2152e16c562cfb3b
Trikyname <alvarocolomvidal@gmail.com>
2020-11-21 12:43:26 +0100

    Hovering Mouse

---
+++ BikiesUnityProject/Assets/ClickManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickManager : MonoBehaviour
+{
+
+    public LayerMask detectionMask;
+    public GameObject lastSelected;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+        RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero, 100f, detectionMask);
+
+        if (hit.collider != null)
+        {
+            Debug.Log(hit.collider.gameObject.name);
+            lastSelected = hit.collider.gameObject.transform.GetChild(0).gameObject;
+            ActivateFloatingMenu(lastSelected);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                // Aqui se comprara una bici
+                Debug.Log("Hey");
+                hit.collider.GetComponent<currentBikes>().AddBike();
+        
+            }
+        }
+        else if (hit.collider == null && lastSelected != null)
+        {
+            DeactivateFloatingMenu(lastSelected);
+        }
+
+
+       
+    }
+
+    void ActivateFloatingMenu(GameObject floatingMenu)
+    {
+        if (!floatingMenu.activeSelf)
+            floatingMenu.SetActive(true);
+    }
+
+    void DeactivateFloatingMenu(GameObject floatingMenu)
+    {
+        if (floatingMenu.activeSelf)
+            floatingMenu.SetActive(false);
+
+        lastSelected = null;
+
+    }
+}
+
---
+++ BikiesUnityProject/Assets/currentBikes.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class currentBikes : MonoBehaviour
+{
+
+    public List<Image> bikeSlots = new List<Image>();
+    public List<Image> freeSlots = new List<Image>();
+
+   // public Image[] bikeSlots;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddBike()
+    {
+        for (int i = 0; i < bikeSlots.Count; i++)
+        {
+            if(bikeSlots[i].tag == "Empty")
+            {
+                bikeSlots[i].color = Color.red;
+                bikeSlots[i].tag = "Full";
+                return;
+            }
+        }
+
+    }
+
+}

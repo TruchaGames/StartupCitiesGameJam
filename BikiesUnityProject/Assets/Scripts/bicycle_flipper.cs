b3d643f0a1529f9bbb698cf852c3a1e8c7cc1659
Dídac Romero <36265669+DidacRomero@users.noreply.github.com>
2020-11-22 02:06:51 +0100

    bicycle rotation iteration

--- BikiesUnityProject/Assets/Scripts/bicycle_flipper.cs
+++ BikiesUnityProject/Assets/Scripts/bicycle_flipper.cs
@@ -14,12 +14,18 @@ public class bicycle_flipper : MonoBehaviour
     public Sprite leftSprite;
     public Sprite rightSprite;
     public Sprite pedestrianSprite;
-
+    public float rotation_time = 1.0f;
    
 
     public bool go_left = true;
+    private bool last_direction = true;
+    private bool changed_direction = false;
+    private bool rotating = false;
+    private bool run_timer = false;
 
     private float last_pos_x = 0.0f;
+    private float time_rot = 0.0f;
+    private float degrees_rot = 0.0f;
 
     private SpriteRenderer renderized_sprite;
     // Start is called before the first frame update
@@ -35,20 +41,22 @@ public class bicycle_flipper : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (go_left)
-            renderized_sprite.sprite = leftSprite;
-        else
-            renderized_sprite.sprite = rightSprite;
+        //if (go_left)
+        //    renderized_sprite.sprite = leftSprite;
+        //else
+        //    renderized_sprite.sprite = rightSprite;
 
         if (unit_type == UNITTYPE.Bicycle)
         {
+            
+            renderized_sprite.sprite = leftSprite;
             if (transform.position.x > last_pos_x)//Going right
             {
-                go_left = false;
+                renderized_sprite.flipX = true;
             }
             else if (transform.position.x < last_pos_x)//Going left
             {
-                go_left = true;
+                renderized_sprite.flipX = false;
             }
         }
         else if(unit_type == UNITTYPE.Pedestrian)
@@ -56,7 +64,33 @@ public class bicycle_flipper : MonoBehaviour
             renderized_sprite.sprite = pedestrianSprite;
         }
 
+        //if (rotating == true)
+        //{
+        //    time_rot += Time.deltaTime;
+
+        //    //Here we lerp the value to rotate
+        //    float lerp_val = time_rot / rotation_time;
+        //    if (lerp_val > 1.0f)
+        //        lerp_val = 1.0f;
+
+        //    //Lerp and rotate
+        //    transform.Rotate(180.0f * Time.deltaTime,0,0);
+
+        //    //We finished rotating
+        //    if (time_rot >= rotation_time)  
+        //    {
+        //        rotating = false;
+        //        time_rot = 0.0f;
+        //        degrees_rot = 0.0f;
+        //    }
+        //}
+
+        //if (last_direction != go_left)
+        //{
+        //    rotating = true;
+        //}
 
         last_pos_x = transform.position.x;
+        last_direction = go_left;
     }
 }
